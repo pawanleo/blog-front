5e14439b5e8ebc34531cc78999cba04a7eb069b1
pawanleo <pawansinghrathore0708@gmail.com>
2022-01-05 11:34:46 +0530

    changes added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,32 +7,13 @@ const Footer = () => {
 
             <div class=" container justify-content-around pt-3 d-flex">
                 <div>
-                    <p>Copyright &#169;2022 All rights reserved | This template is made with 💖 by <span
+                    <p>Copyright &#169;2022 All rights reserved <span
                         style={{ 'font-weight': 'bold', 'cursor': 'pointer', 'color': '#fb1239' }}>
-                        ShanuTech
+                      
                     </span>
                     </p>
                 </div>
-                <div>
-
-
-                    <a title="twitter" href="https://twitter.com/ShaikhS78196343" target="_blank"><i
-                        class="fab fa-twitter i-color "></i></a>
-                    <a title="facebook" href="https://www.facebook.com/profile.php?id=100030898398832" target="_blank"><i
-                        class="fab fa-facebook i-color mx-3"></i></a>
-
-                    <a title="instagram" href="https://www.instagram.com/__shah_nawaz__/" target="_blank"><i
-                        class="fab fa-instagram i-color "></i></a>
-
-                    <a title="linkedIn" href="https://www.linkedin.com/in/shaikh-mohammed-shahnawaz-9149601b5/" target="_blank"><i
-                        class="fab fa-linkedin i- mx-3 "></i></a>
-
-                    <a title="github" href="https://github.com/Shaikh-Shahnawaz" target="_blank"><i
-                        class="fab fa-github i-color "></i></a>
-
-                    <a title="stackoverflow" href="https://stackoverflow.com/users/14162052/shaikh-shahnawaz"
-                        target="_blank"><i class="fab fa-stack-overflow i-color ms-3 "></i></a>
-                </div>
+              
             </div>
 
         </div >
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <div>
             <Navbar style={{'backgroundColor':'#fb1239'}} fixed="top"  variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#">Shanu Blog</Navbar.Brand>
+                    <Navbar.Brand href="#"> Blog</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
